c2a05275c15cb05ac00238b3f797acad941f20ae
Sugam K S <sugamprasadks@gmail.com>
2022-10-11 17:41:16 +0530

    changes in MainActivity

--- app/src/main/java/com/example/message/MainActivity.java
+++ app/src/main/java/com/example/message/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         if (! Python.isStarted()) {
             Python.start(new AndroidPlatform(this));
         }
@@ -25,13 +24,15 @@ public class MainActivity extends AppCompatActivity {
         final EditText message = messageswindow.getWritingMessageView().findViewById(com.samsao.messageui.R.id.message_box_text_field);
         message.setHint("Type here ...");
         messageswindow.setBackgroundResource(R.color.colorPrimaryDark);
+        messageswindow.receiveMessage("Hi, Welcome to Chatbot for E-commerce.");
+        messageswindow.receiveMessage("My name is EDITH");
         messageswindow.getWritingMessageView().findViewById(com.samsao.messageui.R.id.message_box_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                messageswindow.sendMessage(message.getText().toString());
                 Python py = Python.getInstance();
+                messageswindow.sendMessage(message.getText().toString());
                 PyObject pyobj = py.getModule("script");
-                PyObject obj = pyobj.callAttr("main",message.getText().toString());
+                PyObject obj = pyobj.callAttr("main",message.getText().toString().toLowerCase());
                 message.setText("");
                 messageswindow.receiveMessage(obj.toString());
             }
